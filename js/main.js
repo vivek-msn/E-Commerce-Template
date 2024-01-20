648fb194af5b4c28a1a2addd15ab6759bc1ed106
Vivek <vivek_msn@hotmail.com>
2024-01-20 17:27:55 +0530

    add scroll code plus js function but some features not work yet

--- js/main.js
+++ js/main.js
@@ -25,6 +25,24 @@ countDown()
 setInterval(countDown, 1000)
 
 // Scroll Back To Top 
+function scrollTopBack(){
+    let scrollTopButton = document.querySelector("#scrollUp");
+    window.onscroll = function (){
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 250){
+            scrollTopButton.classList.add('scrollActive');
+        }else{
+            scrollTopButton.classList.remove('scrollActive');
+        }
+    }
+}
+scrollTopBack();
 
-
-// Nav Hide 
+// nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
